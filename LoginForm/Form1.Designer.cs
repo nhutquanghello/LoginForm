f75cf163ddbe7bc186a597c297827548c151f4bd
kevinquang1699 gmail.com <kevinquang1699@gmail.com>
2021-03-21 12:48:56 +0700

    Add project files.

---
+++ LoginForm/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace LoginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.btn_thoat = new System.Windows.Forms.Button();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // btn_dangnhap
+            // 
+            this.btn_dangnhap.Location = new System.Drawing.Point(167, 209);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(75, 23);
+            this.btn_dangnhap.TabIndex = 2;
+            this.btn_dangnhap.Text = "Đăng nhập";
+            this.btn_dangnhap.UseVisualStyleBackColor = true;
+            this.btn_dangnhap.Click += new System.EventHandler(this.btn_dangnhap_Click);
+            // 
+            // btn_thoat
+            // 
+            this.btn_thoat.Location = new System.Drawing.Point(298, 209);
+            this.btn_thoat.Name = "btn_thoat";
+            this.btn_thoat.Size = new System.Drawing.Size(75, 23);
+            this.btn_thoat.TabIndex = 3;
+            this.btn_thoat.Text = "Thoát";
+            this.btn_thoat.UseVisualStyleBackColor = true;
+            this.btn_thoat.Click += new System.EventHandler(this.btn_thoat_Click);
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(198, 106);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(157, 20);
+            this.txt_username.TabIndex = 4;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(198, 156);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(157, 20);
+            this.txt_password.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 355);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.btn_thoat);
+            this.Controls.Add(this.btn_dangnhap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_dangnhap;
+        private System.Windows.Forms.Button btn_thoat;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+    }
+}
+
---
+++ LoginForm/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_username.Focus(); //Đưa con trỏ hiển thị ngay textbox username
+            txt_password.Focus(); //Đưa con trỏ hiển thị ngay textbox password
+        }
+
+        private void btn_thoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dangnhap()
+        {
+            if (txt_username.Text.Length == 0 && txt_password.Text.Length == 0)
+                MessageBox.Show("Mời bạn đăng nhập!!!");
+            else
+                if (this.txt_username.Text.Length == 0)
+                MessageBox.Show("Bạn chưa đăng nhập Username!!!");
+            else
+                if (this.txt_password.Text.Length == 0)
+                MessageBox.Show("Bạn chưa đăng nhập Password!!!");
+            else
+                if (this.txt_username.Text == "nhutquang" && this.txt_password.Text == "123456")
+                MessageBox.Show("Đăng nhập thành công");
+            else
+                MessageBox.Show("Mật khẩu của bạn không đúng");
+        }
+
+        private void btn_dangnhap_Click(object sender, EventArgs e)
+        {
+            Form2 fm = new Form2();
+            if (this.txt_username.Text == "nhutquang" && this.txt_password.Text == "123456")
+            {
+                fm.Show();
+            }
+            dangnhap();
+        }
+    }
+}
